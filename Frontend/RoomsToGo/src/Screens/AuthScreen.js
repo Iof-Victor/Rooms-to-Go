a58e3c86b2beb34cd17b516e8264f90929734ab6
Victor Ioftor <vioftor@gmail.com>
2022-12-13 00:53:49 +0200

    register and login are complete

--- Frontend/RoomsToGo/src/Screens/AuthScreen.js
+++ Frontend/RoomsToGo/src/Screens/AuthScreen.js
@@ -1,20 +1,136 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const AuthScreen = () => {
+const AuthScreen = props => {
+  const {navigation} = props;
   const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   return (
-    <View>
-      <TextInput
-        value={email}
-        placeholder="Email"
-        keyboardType="email-address"
-      />
-      <Text>Auth Screen</Text>
+    <View style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('../assets/imagines/logo.png')}
+          style={styles.logo}
+        />
+      </View>
+      <View style={styles.inputView}>
+        <View style={styles.emailIcon}>
+          <MaterialCommunityIcons name="email" size={40} color="#D9D9D9" />
+        </View>
+        <TextInput
+          value={email}
+          placeholder="Email"
+          style={styles.textInput}
+          keyboardType="email-address"
+        />
+      </View>
+      <View style={styles.inputView}>
+        <View style={styles.passwordIcon}>
+          <MaterialCommunityIcons name="lock" size={40} color="#D9D9D9" />
+        </View>
+        <TextInput
+          value={password}
+          placeholder="Password"
+          style={styles.textInput}
+          secureTextEntry
+        />
+      </View>
+      <View>
+        <TouchableOpacity
+          style={styles.logInButton}
+          onPress={() => navigation.navigate('My Account')}>
+          <Text style={styles.logInText}>Log In</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <TouchableOpacity
+          style={styles.registerContainer}
+          onPress={() => navigation.navigate('Register')}>
+          <Text style={styles.registerTextContainer}>
+            No account? Sign up here
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: -40,
+    alignItems: 'center',
+    right: 120,
+  },
+  logo: {
+    resizeMode: 'contain',
+    height: 300,
+    width: 200,
+  },
+  inputView: {
+    marginBottom: 10,
+    marginTop: 10,
+    alignItems: 'center',
+  },
+  textInput: {
+    backgroundColor: '#D9D9D9',
+    height: 70,
+    width: 350,
+    borderRadius: 20,
+    alignItems: 'center',
+    paddingLeft: 20,
+    marginLeft: 15,
+  },
+  logInButton: {
+    backgroundColor: '#9EB7B8',
+    marginTop: 20,
+    width: 150,
+    borderRadius: 15,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logInText: {
+    alignItems: 'center',
+    fontSize: 15,
+    color: 'white',
+  },
+  registerContainer: {
+    position: 'absolute',
+    top: 190,
+    left: -85,
+  },
+  registerTextContainer: {
+    fontSize: 16,
+    alignItems: 'center',
+    fontWeight: '300',
+  },
+  passwordIcon: {
+    position: 'absolute',
+    top: 6,
+    right: 345,
+    padding: 10,
+  },
+  emailIcon: {
+    position: 'absolute',
+    top: 6,
+    right: 345,
+    padding: 10,
+  },
+});
 
 export default AuthScreen;
---
+++ Frontend/RoomsToGo/src/Screens/MyAccountScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const MyAccountScreen = props => {
+  const {navigation} = props;
+  return (
+    <View>
+      <Text>My Account</Text>
+    </View>
+  );
+};
+
+const style = StyleSheet.create({});
+
+export default MyAccountScreen;
