f1d1eb93f3918e4950743fbdcc6e62d40026f158
Victor Ioftor <vioftor@gmail.com>
2022-12-19 20:53:48 +0200

    checkout created

---
+++ Frontend/RoomsToGo/src/Screens/CheckOutScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const CheckOutScreen = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CheckOutScreen;
