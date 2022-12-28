ba02af1d600219daa0bfc53cd01a79c976a4e058
raulpopovici <raulpopovicilucian@gmail.com>
2022-12-28 20:09:46 +0200

    register screen is done

--- Frontend/RoomsToGo/index.js
+++ Frontend/RoomsToGo/index.js
@@ -5,5 +5,8 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import axios from 'axios';
 
+axios.defaults.baseURL = 'http://localhost:8080/api';
+axios.defaults.withCredentials = true;
 AppRegistry.registerComponent(appName, () => App);
